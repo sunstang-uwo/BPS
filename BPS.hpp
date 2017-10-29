318f7901e91c788f0f33946a3e316f4b909070f3
Justin Butler <hockey-star55@hotmail.com>
2017-10-29 09:40:28 -0600

    Removed most code to allow for changes to main board

--- BPS.hpp
+++ BPS.hpp
@@ -1,23 +1,23 @@
-//
-//  main.hpp
-//  Sunstang 2018 BPS
-//
-//  Created by Justin Butler on 2017-08-20.
-//  Copyright © 2017 Justin Butler. All rights reserved.
-//
 
-#ifndef main_hpp
-#define main_hpp
+//  BPS.hpp
+//  Sunstang 2018 BPS
+
+#ifndef BPS_hpp
+#define BPS_hpp
 
 #include <stdio.h>
-#include <TimerOne.h>
 #include <Wire.h>
+#include <LiquidCrystal.h>
+#include <TimerOne.h>
+#include <mcp_can.h>
+#include <mcp_can_dfs.h>
+#include <SPI.h>
 
-#endif /* main_hpp */
+#endif /* BPS_hpp */
 
 using namespace std;
 
-// CONSTANT DECLARATIONS
+// CONSTANT BPS LIMITS - Set from Battery Datasheet
 // TODO: Replace constants below with actual values
 const float OVER_TEMPERATURE_DISCHARGE = 00;
 const float OVER_TEMPERATURE_CHARGING = 00;
@@ -27,6 +27,7 @@ const float OVER_CURRENT_CHARGING = 00;
 
 const float OVER_VOLTAGE = 00;
 const float UNDER_VOLTAGE = 00;
+
 
 //Battery Pack Status Codes
 const int STATUS_CODE_SAFE_TO_USE = 0;
@@ -38,139 +39,42 @@ const int STATUS_CODE_OVERCURRENT_CHARGING = 4;
 const int STATUS_CODE_OVERVOLTAGE = 5;
 const int STATUS_CODE_UNDERVOLTAGE = 6;
 
-const int STATUS_CODE_SOFTWARE_ERROR = 98; // If raised, likely bug in code
+const int STATUS_CODE_SOFTWARE_ERROR = 98; // If raised, likely bug in code (is this neccesary?)
 const int STATUS_CODE_HARDWARE_ERROR = 99; //TODO: Maybe use a smaller number?
 
 
 //GLOBAL VARIABLES
-int BPS_STATUS; //0 on startup, 1 when activated, 9 on BPS trip
+int CAN_STATUS;
 int TEMP_SENSOR_NUMBER = 8; //Number of temperature sensor boards
 const int PERIOD = 1000000; //10Hz
 
-volatile boolean task_flag = false;
 enum measurement {voltage, current, temperature};
 enum CARSTATE {CAR_IDLE, CAR_ACTIVE};
+enum BPS_STATUS {BPS_OFF, BPS_ACTIVE, BPS_TRIP};
+enum CHARGING_STATUS {CHARGING, DISCHARGING};
 
-// BOARD INITIALIZATION
-class StartUp {
+
+class main_board {
 public:
-    void wait_on_startup(){ //Initial function call, will handle all initialization and set BPS_STATUS to 1
+    //INITIALIZATION
+    void init(){
         // Set BPS_STATUS to 0
-        BPS_STATUS = 0;
+        volatile BPS_STATUS bps_stat = BPS_OFF;
+        CAN_STATUS = 0;
         volatile CARSTATE car_state = CAR_IDLE;
         Serial.println("Initializing BPS...");
         
-        
-        while (BPS_STATUS != 1) {
-            init_master();
-            
-        }
-    };
-    
-    void init_master(){
         analogWrite(8, 123); //Set fan duty cycle to 50%
         
         Timer1.initialize(PERIOD);
-        
+        //Timer1.attachInterrupt(timerISR);
         Serial.begin(9600);
-        //Wire.begin(); TODO: Uncomment
-        
-    };
-    
-    void init_temp(){
-        Serial.begin(9600);
-    };
-    
-    void init_current(){
-        
-    };
-    
-    void init_can(){
-        
-    };
-};
-
-//CLASS DEFINITIONS
-// Use a class to handle sensor measurements for temperature, current and voltage
-// Will read sensor measurements in voltage and convert to final values
-class Sensor {
-    double adc_value;
-    double final_value;
-    int sensor_address;
-    char type; // Will dictate if measurement is temp, current or volt.
-    
-private:
-    
-    
-    double calculate_final_value(){
-        if (type == temperature){
-            // Calculate temperature from thermistor value
-            //TODO: Add temp. calculations
-            final_value = 0;}
-        else if (type == current){
-            // Calculate the current from the current sensor
-            //TODO: Add current calculations
-            final_value = 0;}
-        else if (type == voltage){
-            // Calculate the voltage
-            //TODO: Add voltage calculations
-            final_value = 0;}
-        else{
+        Serial.println("Initializing CAN...");
+        for (int i=0; i<5 && CAN_STATUS==0; i++) {
             
-            return BPS_STATUS = STATUS_CODE_SOFTWARE_ERROR;}
-        
-        return final_value;
-    }
-    
-public:
-    Sensor(measurement type, int); //Constructor
-    
-    double read_sensor(){
-        return adc_value;
-    }
-    
-};
-
-//Sensor class constructor. Sets sensor address ID on initialization
-Sensor::Sensor(enum measurement measurement, int id){
-    sensor_address = id;
-    type = measurement;
-}
-
-//TASK HANDLING
-class Tasks {
-    
-public:
-    void timerISR(){
-        task_flag = true;
-    }
-    
-    void WaitForFlag(){
-        while (task_flag == false);
-        task_flag = false; //resets flag after interrupt is finished
-    }
-    
-    void Task_CheckVoltage(){
-    
-    }
-    
-    void Task_CheckTemp(){
-    
-    }
-    
-    void Task_CheckCurrent(){
-
-    }
-    
-    void Task_UpdateLCD(){
-        
-    }
-    
-    void Task_SendCAN(){
-        
+        }
     }
 };
 
-// ERROR HANDLING
 
 
--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 SunStang's 2018 Battery Protection System
 Designed for the team's first cruiser style vehicle
 
+The main board is controlled by a custom Arduino Mega and receives current and temperature sensor measurements in the form of voltages. This will reduce the amount of coding and hardware required compared to previous BPS' systems using multiple Arduino Nanos.
 
+The main board handles the following tasks:
+- Initialize CAN to communicate with telemetry (Uses the MCP2515/MCP2551 chips & mcp_can.h)
+- Send out voltage/current/temperature values on CAN to telemtery at ??? rate
+- Activate the pack fans (@ 50% duty cycle?)
+- Initially read the current, voltage and temperature on start-up before activating the main batteries
+- Read the current sensor voltage and calculate the equivalent current (Pin ???)
+- Read the temperature board voltage, keep track of which module it is reading, and calculate the equivalent temperature, control the board multiplexer (Pins ???)
+- Read the voltage board values using SPI (Using LT DC1894 & DC1941 dev boards) (Pins ???)
+- Activate the relays to turn on/off the array, motor and car based on BPS status and voltage/current levels
+- Shut down the car when BPS faults occur
+- Initialize and update the battery box/driver display LCD (Could have individual LCD in battery box for current, temp and voltage )
+- Update the LCD display with BPS status and current/voltage/temp values
+- Read voltage/temp/current at rate of ???
+- Handles reading all neccesary measurements using task handling???
+-
